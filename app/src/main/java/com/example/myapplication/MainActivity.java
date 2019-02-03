8537d1c9d8bae945d6facb3680ac52419191acd8
Sankalp <sankuracecar@gmail.com>
2019-02-02 18:56:15 -0600

    Finished the main xml file with out the json file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,12 +14,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        LinearLayout Layout = (LinearLayout) findViewById(R.id.Scroll_Screen);
+//        LayoutInflater li =  (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        JSONObject products = new JSONObject(readJson());
+//
+//        for (int i = 0; i < mainList.size();  i++){
+//            View tempView = li.inflate(R.layout.listtemplate, null);
+//            TextView textMain = (TextView) tempView.findViewById(R.id.mainText);
+//
+//            textMain.setText(mainList.get(i));
+//            mainLayout.addView(tempView);
+//        }
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -94,5 +123,23 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private String readJson() {
+        String json;
+        try{
+            InputStream reader = getAssets().open("test.json");
+            int size = reader.available();
+            byte[] buffer = new byte[size];
+            reader.read(buffer);
+            reader.close();
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
