cacb8376dea2f9998f470786c118b0437c1ee3bf
Sankalp <sankuracecar@gmail.com>
2019-02-02 19:50:05 -0600

    Added Main activity working on the connections page

---
+++ app/src/main/java/com/example/myapplication/Connection.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+public class Connection extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+}
